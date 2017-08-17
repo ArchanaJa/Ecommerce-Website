d7bcd7c3e46862a2cd9747156a8684437c661424
ArchanaJa <archu5432@gmail.com>
2017-08-17 16:27:11 +0100

    Add files via upload

---
+++ Extensions/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicsShop.Extensions
+{
+    public static class Utils
+    {
+        public static bool IsAdmin(this HtmlHelper html)
+        {
+            HttpContext context = HttpContext.Current;
+            return context.Session["Role"] != null && context.Session["Role"].ToString() == "admin";
+        }
+    }
+}
